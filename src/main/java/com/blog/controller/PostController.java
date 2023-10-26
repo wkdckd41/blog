31fa65240a6badfc11bbfeb1a74a9cb92a815620
wkdckd41 <wkdckd41@gmail.com>
2023-10-26 23:46:22 +0900

    섹션1 블로그 API만들기 기본 - 데이터 검증-2

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -2,15 +2,11 @@ package com.blog.controller;
 
 import com.blog.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -18,17 +14,7 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firstFieldError = fieldErrors.get(0);
-            String fieldName = firstFieldError.getField(); //title
-            String errorMessage = firstFieldError.getDefaultMessage(); // ..에러 메시지
-
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
+    public Map<String, String> post(@RequestBody @Valid PostCreate params) {
         return Map.of();
     }
 }
