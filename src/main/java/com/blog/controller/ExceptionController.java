40baaa35e9564bb56af9aeb0104f16b904fdc96e
wkdckd41 <wkdckd41@gmail.com>
2023-10-27 23:24:46 +0900

    섹션1 블로그 API만들기 기본 - 데이터 검증-2

--- src/main/java/com/blog/controller/ExceptionController.java
+++ src/main/java/com/blog/controller/ExceptionController.java
@@ -1,16 +1,31 @@
 package com.blog.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @ControllerAdvice
 public class ExceptionController {
 
-    @ResponseStatus(HttpStatus.OK)
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler() {
-        System.out.println("하하하");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
+
+        Map<String, String> response = new HashMap<>();
+        response.put(field, message);
+        return response;
     }
 }
