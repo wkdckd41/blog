1dac424136492ac8d9121ab8945d1a9b7f5c0082
wkdckd41 <wkdckd41@gmail.com>
2023-12-19 20:53:44 +0900

    섹션2 Spring REST Docs1 - 기본설정

--- src/test/java/com/blog/api/controller/PostControllerDocTest.java
+++ src/test/java/com/blog/api/controller/PostControllerDocTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static com.querydsl.core.alias.MethodType.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
